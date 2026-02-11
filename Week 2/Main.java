595e48a1eaaf10d322eaadd5637e7e90dd756ee0
vtu26125 <vtu26125@veltech.edu.in>
2026-02-11 14:55:43 +0530

    Add files via upload

---
+++ Week 2/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index < arr.length) {
+            System.out.println("Element at index " + index + " is: " + arr[index]);
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+}
